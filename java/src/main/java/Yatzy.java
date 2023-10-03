b16548be35a798c3ae4cf024d879e9ec4fa384ba
amani <amaniboudhri@gmail.com>
2023-10-03 11:56:39 +0200

    refactor threes method

--- java/src/main/java/Yatzy.java
+++ java/src/main/java/Yatzy.java
@@ -25,18 +25,10 @@ public class Yatzy {
 
     public static int twos(DiceRoller diceRoller) {
         return getDiceNumberCount(diceRoller, 2);
-
     }
 
-    public static int threes(int d1, int d2, int d3, int d4, int d5) {
-        int s;
-        s = 0;
-        if (d1 == 3) s += 3;
-        if (d2 == 3) s += 3;
-        if (d3 == 3) s += 3;
-        if (d4 == 3) s += 3;
-        if (d5 == 3) s += 3;
-        return s;
+    public static int threes(DiceRoller diceRoller) {
+        return getDiceNumberCount(diceRoller, 3);
     }
 
     public static int fours(int d1, int d2, int d3, int d4, int d5) {
